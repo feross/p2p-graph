de2591a5999d15eab0e3982d3b56b118fa5b11db
Feross Aboukhadijeh <feross@feross.org>
2016-09-20 18:45:45 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# p2p-graph [![travis][travis-image]][travis-url] [![npm][npm-image]][npm-url] [![downloads][downloads-image]][downloads-url]
+
+[travis-image]: https://img.shields.io/travis/feross/p2p-graph/master.svg
+[travis-url]: https://travis-ci.org/feross/p2p-graph
+[npm-image]: https://img.shields.io/npm/v/p2p-graph.svg
+[npm-url]: https://npmjs.org/package/p2p-graph
+[downloads-image]: https://img.shields.io/npm/dm/p2p-graph.svg
+[downloads-url]: https://npmjs.org/package/p2p-graph
+
+### Real-time P2P network visualization with D3
+
+You can see this package in action on the [webtorrent.io](https://webtorrent.io/)
+homepage.
+
+Works in the browser with [browserify](http://browserify.org/)! This module is used by [WebTorrent](http://webtorrent.io).
+
+## install
+
+```
+npm install p2p-graph
+```
+
+## usage
+
+```js
+var Graph = require('p2p-graph')
+
+var graph = new Graph('.root')
+
+// Add two peers
+graph.add({
+  id: 'peer1',
+  me: true,
+  name: 'You'
+})
+graph.add({
+  id: 'peer2',
+  name: 'Another Peer'
+})
+
+// Connect them
+graph.connect('peer1', 'peer2')
+```
+
+## api
+
+### graph = new Graph(rootElem)
+
+Create a new P2P graph at the root DOM element `rootElem`. In addition to an
+`Element`, a query selector string (like `'.my-cool-element'`) can also be passed
+in.
+
+### graph.add(peer)
+
+Add a peer to the graph. The `peer` object should contain:
+
+```
+{
+  id: 'unique-identifier', // should be unique across all peers
+  me: true, // is this the current user?
+  name: 'display name' // name to show in the graph UI
+}
+
+### graph.connect(id1, id2)
+
+Connect to two nodes, identified by `id1` and `id2`, to each other.
+
+### graph.disconnect(id1, id2)
+
+Disconnect two nodes, identified by `id1` and `id2`, from each other.
+
+### graph.remove(id)
+
+Remove a node, identified by `id`, from the graph.
+
+## license
+
+MIT. Copyright (c) [Feross Aboukhadijeh](http://feross.org).
---
+++ index.js
@@ -0,0 +1,329 @@
+module.exports = TorrentGraph
+
+var d3 = require('d3')
+var debug = require('debug')('webtorrent-www:torrent-graph')
+var debounce = require('debounce')
+
+var COLORS = {
+  links: '#FAFAFA',
+  text: {
+    subtitle: '#FAFAFA'
+  },
+  nodes: {
+    method: function (d, i) {
+      return d.me
+        ? d3.hsl(210, 0.7, 0.725) // blue
+        : d.seeder
+          ? d3.hsl(120, 0.7, 0.725) // green
+          : d3.hsl(55, 0.7, 0.725) // yellow
+    },
+    hover: '#FAFAFA',
+    dep: '#252929'
+  }
+}
+
+function TorrentGraph (root) {
+  if (typeof root === 'string') root = document.querySelector(root)
+  var model = {
+    nodes: [],
+    links: []
+  }
+
+  function scale () {
+    var len = model.nodes.length
+    return len < 10
+      ? 1
+      : Math.max(0.2, 1 - ((len - 10) / 100))
+  }
+
+  var width = root.offsetWidth
+  var height = (window.innerWidth >= 900) ? 400 : 250
+
+  var focus
+
+  model.links.forEach(function (link) {
+    var source = model.nodes[link.source]
+    var target = model.nodes[link.target]
+
+    source.children = source.children || []
+    source.children.push(link.target)
+
+    target.parents = target.parents || []
+    target.parents.push(link.source)
+  })
+
+  var svg = d3.select(root).append('svg')
+    .attr('width', width)
+    .attr('height', height)
+
+  var force = d3.layout.force()
+    .size([width, height])
+    .nodes(model.nodes)
+    .links(model.links)
+    .on('tick', function () {
+      link
+        .attr('x1', function (d) { return d.source.x })
+        .attr('y1', function (d) { return d.source.y })
+        .attr('x2', function (d) { return d.target.x })
+        .attr('y2', function (d) { return d.target.y })
+
+      node
+        .attr('cx', function (d) { return d.x })
+        .attr('cy', function (d) { return d.y })
+
+      node.attr('transform', function (d) { return 'translate(' + d.x + ',' + d.y + ')' })
+    })
+
+  var node = svg.selectAll('.node')
+  var link = svg.selectAll('.link')
+
+  update()
+
+  function update () {
+    link = link.data(model.links)
+    node = node.data(model.nodes, function (d) { return d.id })
+
+    link.enter()
+      .insert('line', '.node')
+        .attr('class', 'link')
+        .style('stroke', COLORS.links)
+        .style('opacity', 0.5)
+
+    link.exit()
+      .remove()
+
+    var g = node.enter()
+      .append('g')
+      .attr('class', 'node')
+
+    g.call(force.drag)
+
+    g.append('circle')
+      .on('mouseover', function (d) {
+        d3.select(this)
+          .style('fill', COLORS.nodes.hover)
+
+        d3.selectAll(childNodes(d))
+          .style('fill', COLORS.nodes.hover)
+          .style('stroke', COLORS.nodes.method)
+          .style('stroke-width', 2)
+
+        d3.selectAll(parentNodes(d))
+          .style('fill', COLORS.nodes.dep)
+          .style('stroke', COLORS.nodes.method)
+          .style('stroke-width', 2)
+      })
+      .on('mouseout', function (d) {
+        d3.select(this)
+          .style('fill', COLORS.nodes.method)
+
+        d3.selectAll(childNodes(d))
+          .style('fill', COLORS.nodes.method)
+          .style('stroke', null)
+
+        d3.selectAll(parentNodes(d))
+          .style('fill', COLORS.nodes.method)
+          .style('stroke', null)
+      })
+      .on('click', function (d) {
+        if (focus === d) {
+          force.charge(-200 * scale())
+            .linkDistance(100 * scale())
+            .linkStrength(1)
+            .start()
+
+          node.style('opacity', 1)
+          link.style('opacity', 0.3)
+
+          focus = false
+
+          return
+        }
+
+        focus = d
+
+        node.style('opacity', function (o) {
+          o.active = connected(d, o)
+          return o.active ? 1 : 0.2
+        })
+
+        force.charge(function (o) {
+          return (o.active ? -100 : -5) * scale()
+        }).linkDistance(function (l) {
+          return (l.source.active && l.target.active ? 100 : 20) * scale()
+        }).linkStrength(function (l) {
+          return (l.source === d || l.target === d ? 1 : 0) * scale()
+        }).start()
+
+        link.style('opacity', function (l, i) {
+          return l.source.active && l.target.active ? 0.2 : 0.02
+        })
+      })
+
+    node.select('circle')
+      .attr('r', function (d) {
+        return scale() * (d.me ? 15 : 10)
+      })
+      .style('fill', COLORS.nodes.method)
+
+    g.append('text')
+      .attr('class', 'text')
+      .text(function (d) { return d.name })
+
+    node.select('text')
+      .attr('font-size', function (d) {
+        return d.me ? 16 * scale() : 12 * scale()
+      })
+      .attr('dx', 0)
+      .attr('dy', function (d) {
+        return d.me ? -22 * scale() : -15 * scale()
+      })
+
+    node.exit()
+      .remove()
+
+    force
+      .linkDistance(100 * scale())
+      .charge(-200 * scale())
+      .start()
+  }
+
+  function refresh (e) {
+    width = root.offsetWidth
+    height = (window.innerWidth >= 900) ? 400 : 250
+
+    force
+      .size([width, height])
+      .resume()
+
+    svg
+      .attr('width', root.offsetWidth)
+      .attr('height', height)
+  }
+
+  function childNodes (d) {
+    if (!d.children) return []
+
+    return d.children
+      .map(function (child) {
+        return node[0][child]
+      }).filter(function (child) {
+        return child
+      })
+  }
+
+  function parentNodes (d) {
+    if (!d.parents) return []
+
+    return d.parents
+      .map(function (parent) {
+        return node[0][parent]
+      }).filter(function (parent) {
+        return parent
+      })
+  }
+
+  function connected (d, o) {
+    return o.id === d.id ||
+    (d.children && d.children.indexOf(o.id) !== -1) ||
+    (o.children && o.children.indexOf(d.id) !== -1) ||
+    (o.parents && o.parents.indexOf(d.id) !== -1) ||
+    (d.parents && d.parents.indexOf(o.id) !== -1)
+  }
+
+  function getNode (id) {
+    for (var i = 0, len = model.nodes.length; i < len; i += 1) {
+      var node = model.nodes[i]
+      if (node.id === id) return node
+    }
+    return null
+  }
+
+  function getNodeIndex (id) {
+    for (var i = 0, len = model.nodes.length; i < len; i += 1) {
+      var node = model.nodes[i]
+      if (node.id === id) return i
+    }
+    return -1
+  }
+
+  function getLink (source, target) {
+    for (var i = 0, len = model.links.length; i < len; i += 1) {
+      var link = model.links[i]
+      if (link.source === model.nodes[source] && link.target === model.nodes[target]) {
+        return link
+      }
+    }
+    return null
+  }
+
+  function getLinkIndex (source, target) {
+    for (var i = 0, len = model.links.length; i < len; i += 1) {
+      var link = model.links[i]
+      if (link.source === model.nodes[source] && link.target === model.nodes[target]) {
+        return i
+      }
+    }
+    return -1
+  }
+
+  function add (node) {
+    debug('add %s %o', node.id, node)
+    if (getNode(node.id)) throw new Error('add: cannot add duplicate node')
+    model.nodes.push(node)
+    update()
+  }
+
+  function remove (id) {
+    debug('remove $s', id)
+    var index = getNodeIndex(id)
+    if (index === -1) throw new Error('remove: node does not exit')
+    model.nodes.splice(index, 1)
+    update()
+  }
+
+  function connect (sourceId, targetId) {
+    debug('connect %s %s', sourceId, targetId)
+    var sourceNode = getNode(sourceId)
+    if (!sourceNode) throw new Error('connect: invalid source id')
+    var targetNode = getNode(targetId)
+    if (!targetNode) throw new Error('connect: invalid target id')
+    if (getLink(sourceNode.index, targetNode.index)) {
+      throw new Error('connect: cannot make duplicate connection')
+    }
+    model.links.push({ source: sourceNode.index, target: targetNode.index })
+    update()
+  }
+
+  function disconnect (sourceId, targetId) {
+    debug('disconnect %s %s', sourceId, targetId)
+    var sourceNode = getNode(sourceId)
+    if (!sourceNode) throw new Error('connect: invalid source id')
+    var targetNode = getNode(targetId)
+    if (!targetNode) throw new Error('connect: invalid target id')
+    var index = getLinkIndex(sourceNode.index, targetNode.index)
+    if (index === -1) throw new Error('disconnect: connection does not exist')
+    model.links.splice(index, 1)
+    update()
+  }
+
+  function unchoke (sourceId, targetId) {
+    debug('unchoke %s %s', sourceId, targetId)
+  }
+
+  function choke (sourceId, targetId) {
+    debug('choke %s %s', sourceId, targetId)
+  }
+
+  window.addEventListener('resize', debounce(refresh, 500))
+  refresh()
+
+  return {
+    add: add,
+    remove: remove,
+    connect: connect,
+    disconnect: disconnect,
+    unchoke: unchoke,
+    choke: choke
+  }
+}
