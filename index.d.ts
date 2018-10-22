ab687b8dcb7fa6fc18ca3319a99230e3dbfced5c
Stephan Hesse <stephan@emliri.com>
2018-10-22 17:34:13 +0200

    Typings file: correct 2 spaces indentation

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,35 @@
 declare module "p2p-graph" {
-    /***
-     * @see https://github.com/feross/p2p-graph#api
-     */
-    type P2pGraphPeer = {
-        id: string // must be unique for this graph
-        me: boolean, // reference
-        name: string // display name
-    }
 
-    type P2pGraphEvent = 'select';
+  /***
+   * @see https://github.com/feross/p2p-graph#api
+   */
+  type P2pGraphPeer = {
+    id: string // must be unique for this graph
+    me: boolean, // reference
+    name: string // display name
+  }
 
-    class P2pGraph {
-        constructor(rootElem: HTMLElement);
+  type P2pGraphEvent = 'select';
 
-        add(peer: P2pGraphPeer);
-        connect(id1: string, id2: string);
-        disconnect(id: string);
-        areConnected(id1: string, id2: string);
-        getLink(i1: string, id2: string);
-        hasPeer(...ids: string[]): boolean;
-        hasLink(id1: string, id2: string);
-        remove(id: string);
-        seed(id: string, isSeeding: boolean);
-        rate(id1: string, id2: string, avgRate: number);
-        list(): P2pGraphPeer[];
-        destroy();
+  class P2pGraph {
+    constructor(rootElem: HTMLElement);
 
-        on(event: P2pGraphEvent, callback: (id: string) => void);
-    }
+    add(peer: P2pGraphPeer);
+    connect(id1: string, id2: string);
+    disconnect(id: string);
+    areConnected(id1: string, id2: string);
+    getLink(i1: string, id2: string);
+    hasPeer(...ids: string[]): boolean;
+    hasLink(id1: string, id2: string);
+    remove(id: string);
+    seed(id: string, isSeeding: boolean);
+    rate(id1: string, id2: string, avgRate: number);
+    list(): P2pGraphPeer[];
+    destroy();
 
-    export = P2pGraph;
+    on(event: P2pGraphEvent, callback: (id: string) => void);
+  }
+
+  export = P2pGraph;
 }
 
