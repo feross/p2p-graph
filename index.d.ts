2a111bafbe7a7ec2b39fd4654449abc889bddd65
Stephan Hesse <stephan@emliri.com>
2018-10-22 17:20:30 +0200

    Add type definitions
    see https://www.typescriptlang.org/docs/handbook/declaration-files/publishing.html

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+declare module "p2p-graph" {
+    /***
+     * @see https://github.com/feross/p2p-graph#api
+     */
+    type P2pGraphPeer = {
+        id: string // must be unique for this graph
+        me: boolean, // reference
+        name: string // display name
+    }
+
+    type P2pGraphEvent = 'select';
+
+    export default class P2pGraph {
+        constructor(rootElem: HTMLElement);
+
+        add(peer: P2pGraphPeer);
+        connect(id1: string, id2: string);
+        disconnect(id: string);
+        areConnected(id1: string, id2: string);
+        getLink(i1: string, id2: string);
+        hasPeer(...ids: string[]): boolean;
+        hasLink(id1: string, id2: string);
+        remove(id: string);
+        seed(id: string, isSeeding: boolean);
+        rate(id1: string, id2: string, avgRate: number);
+        list(): P2pGraphPeer[];
+        destroy();
+
+        on(event: P2pGraphEvent, callback: (id: string) => void);
+    }
+
+}
+
